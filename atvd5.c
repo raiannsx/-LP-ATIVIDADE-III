9e479effef22d117096f6872a7b4e69e696a1bf7
raiannsx <143418422+raiannsx@users.noreply.github.com>
2023-11-25 22:39:11 -0300

    atvd5.c

--- atvd5.c
+++ atvd5.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
 
+    struct Venda {
+        
+    int main()
+        
+    char produto[50];
+    float preço;
+    int qualidade;
+    char opcao
+    int i;
+    
+    for(i < 0; i < 50; i ++)
+    
+    {
+        
+        printf("n/mostre os produtos vendidos:", i + 1);
+        scanf("%d", &vendaprodutos[i]);
+        
+        printf("Digite o preço do produto: ");
+        scanf("%f", &vendapreco[i]);
+
+    printf("Digite a quantidade vendida: ");
+    scanf("%d", &vendaquantidade[1]);
+    }
+
+    while (1) {
+        printf("\n1 - Adicionar venda\n");
+        printf("2 - Exibir total de vendas e sair\n");
+
+        int opcao;
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        
+
+    return 0;
+    }
